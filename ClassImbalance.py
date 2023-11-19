f114d1e1b92504a6c6f2aab5f375bff0358a06b3
AdesinaBernard <85597466+AdesinaBernard@users.noreply.github.com>
2023-11-19 18:04:29 +0100

    Checking imbalances and created new category

--- ClassImbalance.py
+++ ClassImbalance.py
@@ -7,3 +7,42 @@ import matplotlib.pyplot as plt
 salaries = pd.read_csv('/Users/mac/Downloads/datasets/ds_salaries_clean.csv')
 print(salaries.head())
 print(salaries.columns)
+
+#Checking for class imbalance using value count, normalize
+print(salaries['Designation'].value_counts(normalize=True))
+
+#using a crosstab
+print(pd.crosstab(salaries['Experience'], salaries['Company_Size']))
+
+print(pd.crosstab(salaries['Designation'], salaries['Company_Size']))
+
+print(pd.crosstab(salaries['Designation'], salaries['Company_Size'], values=salaries['Salary_USD'], aggfunc='mean'))
+
+#Extracting features for correlation
+#salaries['month'] = salaries['date_of_response'].dt.month
+#salaries['weekday'] = salaries['date_of_response'].dt.weekday
+
+#sns.heatmap(salaries.corr(), annot=True)
+#plt.show()
+
+#Calculating Salary Percentile
+twenty_fifth = salaries['Salary_USD'].quantile(0.25)
+salary_median = salaries['Salary_USD'].median()
+seventy_fifth = salaries['Salary_USD'].quantile(0.75)
+
+print(twenty_fifth,salary_median,seventy_fifth)
+
+#Categorizing salaries
+#Create salary labels
+salary_labels = ['Entry','Mid','Senior', 'Exec']
+#Create the salary range list
+salary_ranges = [0,twenty_fifth,salary_median,seventy_fifth,salaries['Salary_USD'].max()]
+#Create the salary level
+salaries['salary_level'] = pd.cut(salaries['Salary_USD'], bins=salary_ranges, labels=salary_labels)
+print(salaries['salary_level'].value_counts())
+
+#Count of salary level at different company sizes
+sns.countplot(data=salaries, x='Company_Size', hue='salary_level')
+plt.title('Salary Levels By Company Size')
+plt.show()
+
